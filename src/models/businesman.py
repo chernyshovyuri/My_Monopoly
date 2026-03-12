bc8f01b79ad3d3e5b67808d6c6f055f208aaef19
Yrii Chernyshov <c300@list.ru>
2026-03-12 22:19:46 +0300

    feat: добавил функии добвления и удаления имущества

--- src/models/businesman.py
+++ src/models/businesman.py
@@ -1,3 +1,6 @@
+from src.models.gameboard import Ownership
+
+
 class Businessman:
 
     def __init__(self, balance):
@@ -8,11 +11,33 @@ class Businessman:
     def get_balance(self) -> int:
         return self.__balance
 
-    def add_ownership(self, ownership):
-        pass
+    def add_ownership(self, ownership: Ownership) -> bool:
+        if not isinstance(ownership, Ownership):  raise TypeError()
 
-    def delete_ownership(self, ownership):
-        pass
+        if ownership.has_owner():  return False
+
+        if ownership in self.__ownerships:  return False
+
+        self.__ownerships.append(ownership)
+        ownership.set_owner(self)
+        return True
+
+
+    def delete_ownership(self, ownership:Ownership) -> bool:
+        if not isinstance(ownership, Ownership): raise TypeError()
+
+        if ownership not in self.__ownerships: return False
+
+        dell_ownership = None
+        for elem in self.__ownerships:
+            if elem == ownership:
+                dell_ownership = elem
+                break
+
+        self.__ownerships.remove(dell_ownership)
+        ownership.unset_owner()
+        return True
+
 
     def increase_balance(self, money): # увеличить баланс
         pass
